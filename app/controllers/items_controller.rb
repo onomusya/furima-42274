b575dfe05d74fa7de156ac3339bded4a8ee6fe0c
onomusya <onomusya@gmail.com>
2025-04-23 18:30:05 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,30 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.user = current_user
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :title, :content, :category_id, :condition_id,
+      :shipping_fee_burden_id, :prefecture_id, :shipping_day_id,
+      :price, :image
+    )
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,6 @@ class User < ApplicationRecord
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[0-9])[a-z0-9]+\z/i }
   validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :last_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+
+  has_many :items
 end
