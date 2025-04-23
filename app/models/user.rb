5c84a1926b6c1ce6d5bf12a32259a0399c282e5f
onomusya <onomusya@gmail.com>
2025-04-23 15:25:35 +0900

    Userのモデル単体テストの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :last_name , presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birthday , presence: true
+  validates :nickname, :email, :password, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday,
+            presence: true
+  validates :email, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
+  validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?[0-9])[a-z0-9]+\z/i }
+  validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ }
+  validates :last_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
 end
