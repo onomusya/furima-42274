2c7ca1621c2f1f1cd0a85f5b992ab53610413f2f
onomusya <onomusya@gmail.com>
2025-04-23 17:33:42 +0900

    テーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, :password, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday,
+  validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday,
             presence: true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[0-9])[a-z0-9]+\z/i }
   validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
