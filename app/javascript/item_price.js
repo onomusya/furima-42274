47cd26e7ed5b5f0a02f10fda336664d8d1f48760
onomusya <onomusya@gmail.com>
2025-04-24 18:18:05 +0900

    手数料自動計算の修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,4 +1,4 @@
-document.addEventListener('turbo:load', () => {
+const price = () => {
   const priceInput = document.getElementById("item-price");
 
   if (!priceInput) return;
@@ -23,4 +23,7 @@ document.addEventListener('turbo:load', () => {
     addTaxDom.innerHTML = tax.toLocaleString();
     profitDom.innerHTML = profit.toLocaleString();
   });
-});
+};
+
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render", price);
