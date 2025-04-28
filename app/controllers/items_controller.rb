95da94c8bcf9cf5a6c1be833d1a03a426e54b6e7
onomusya <onomusya@gmail.com>
2025-04-28 15:55:26 +0900

    Rubocopを実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -48,11 +48,10 @@ class ItemsController < ApplicationController
   def set_item
     @item = Item.find(params[:id])
   end
-  
 
   def move_to_index
-    unless user_signed_in? && current_user.id == @item.user_id
-      redirect_to root_path
-    end
+    return if user_signed_in? && current_user.id == @item.user_id
+
+    redirect_to root_path
   end
 end
